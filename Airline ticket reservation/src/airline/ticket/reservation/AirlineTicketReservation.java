6077a34f8f9ce9dddfeaf8686cb88c53e6864246
User <User@DESKTOP-HE4RHMT.stdmlb01.sliit.lk>
2017-05-02 15:50:30 +0530

    test method from manash

---
+++ Airline ticket reservation/src/airline/ticket/reservation/AirlineTicketReservation.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package airline.ticket.reservation;
+
+/**
+ *
+ * @author User
+ */
+public class AirlineTicketReservation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Test");
+        
+    }
+    
+}
