996ecd8a478753fa7a23e161625b13669c133fe7
CSAbeywickrame <chaithikastephen1@gmail.com>
2017-05-10 20:32:37 +0530

    Getters and setters created for class airport

--- air_ticket_reservation/src/Classes/Airport.java
+++ air_ticket_reservation/src/Classes/Airport.java
@@ -10,5 +10,97 @@ package Classes;
  * @author Manash Gurudeniya
  */
 public class Airport {
+    private String airportID;
+    private String name;
+    private String city;
+    private String apstate;
+    private String country;
+    private boolean active;
+
+    /**
+     * @return the airportID
+     */
+    public String getAirportID() {
+        return airportID;
+    }
+
+    /**
+     * @param airportID the airportID to set
+     */
+    public void setAirportID(String airportID) {
+        this.airportID = airportID;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the city
+     */
+    public String getCity() {
+        return city;
+    }
+
+    /**
+     * @param city the city to set
+     */
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    /**
+     * @return the apstate
+     */
+    public String getApstate() {
+        return apstate;
+    }
+
+    /**
+     * @param apstate the apstate to set
+     */
+    public void setApstate(String apstate) {
+        this.apstate = apstate;
+    }
+
+    /**
+     * @return the country
+     */
+    public String getCountry() {
+        return country;
+    }
+
+    /**
+     * @param country the country to set
+     */
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    /**
+     * @return the active
+     */
+    public boolean isActive() {
+        return active;
+    }
+
+    /**
+     * @param active the active to set
+     */
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+    
+    
     
 }
