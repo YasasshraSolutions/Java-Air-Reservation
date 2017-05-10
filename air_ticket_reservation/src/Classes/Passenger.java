d3a265c1e02a4c577e99ee778d0d5b03cdb2b971
CSAbeywickrame <chaithikastephen1@gmail.com>
2017-05-10 20:37:24 +0530

    Getters and setters created for class passenger

--- air_ticket_reservation/src/Classes/Passenger.java
+++ air_ticket_reservation/src/Classes/Passenger.java
@@ -10,5 +10,126 @@ package Classes;
  * @author Shaveen Dimasha
  */
 public class Passenger {
+    private String tel;
+    private String paddress;
+    private String fname;
+    private String lname;
+    private String pass_no;
+    private String password;
+    private String dob;
+    private boolean active;
+
+    /**
+     * @return the tel
+     */
+    public String getTel() {
+        return tel;
+    }
+
+    /**
+     * @param tel the tel to set
+     */
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    /**
+     * @return the paddress
+     */
+    public String getPaddress() {
+        return paddress;
+    }
+
+    /**
+     * @param paddress the paddress to set
+     */
+    public void setPaddress(String paddress) {
+        this.paddress = paddress;
+    }
+
+    /**
+     * @return the fname
+     */
+    public String getFname() {
+        return fname;
+    }
+
+    /**
+     * @param fname the fname to set
+     */
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    /**
+     * @return the lname
+     */
+    public String getLname() {
+        return lname;
+    }
+
+    /**
+     * @param lname the lname to set
+     */
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    /**
+     * @return the pass_no
+     */
+    public String getPass_no() {
+        return pass_no;
+    }
+
+    /**
+     * @param pass_no the pass_no to set
+     */
+    public void setPass_no(String pass_no) {
+        this.pass_no = pass_no;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the dob
+     */
+    public String getDob() {
+        return dob;
+    }
+
+    /**
+     * @param dob the dob to set
+     */
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    /**
+     * @return the active
+     */
+    public boolean isActive() {
+        return active;
+    }
+
+    /**
+     * @param active the active to set
+     */
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+    
     
 }
