8a7d272517757d490cc173ba3fb821659b24d4dc
BuddhiAbeyratne <buddhi741@gmail.com>
2017-05-10 15:48:22 +0530

    Created Db connect

--- air_ticket_reservation/src/Classes/DBConnect.java
+++ air_ticket_reservation/src/Classes/DBConnect.java
@@ -5,10 +5,25 @@
  */
 package Classes;
 
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+
 /**
  *
  * @author Chaithika Stephen
  */
 public class DBConnect {
+ 
+    public static Connection connect()
+    {
+        Connection conn = null;
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/air-ticke","root","");
+        } catch (Exception e) {
+        }
+        return conn;
+    }
     
 }
