c301cb817fc10de4cf3bbe331963839a161029de
CSAbeywickrame <chaithikastephen1@gmail.com>
2017-05-10 20:54:57 +0530

    Getters and setters created for class FlightLeg

---
+++ air_ticket_reservation/src/Classes/FlightLeg.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+/**
+ *
+ * @author Chaithika
+ */
+public class FlightLeg {
+    private String leg_no;
+    private String leg_type;
+
+    /**
+     * @return the leg_no
+     */
+    public String getLeg_no() {
+        return leg_no;
+    }
+
+    /**
+     * @param leg_no the leg_no to set
+     */
+    public void setLeg_no(String leg_no) {
+        this.leg_no = leg_no;
+    }
+
+    /**
+     * @return the leg_type
+     */
+    public String getLeg_type() {
+        return leg_type;
+    }
+
+    /**
+     * @param leg_type the leg_type to set
+     */
+    public void setLeg_type(String leg_type) {
+        this.leg_type = leg_type;
+    }
+    
+    
+    
+}
