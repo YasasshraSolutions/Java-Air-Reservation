102833bf78f6acc03798a66a47cf99fcc062d784
CSAbeywickrame <chaithikastephen1@gmail.com>
2017-05-10 20:44:39 +0530

    Getters and setters created for class tickets

---
+++ air_ticket_reservation/src/Classes/Tickets.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+/**
+ *
+ * @author Chaithika
+ */
+public class Tickets {
+    private String ticket_no;
+    private int seat_no;
+    private float fair;
+    private String pass_no;
+    private String leg_no;
+
+    /**
+     * @return the ticket_no
+     */
+    public String getTicket_no() {
+        return ticket_no;
+    }
+
+    /**
+     * @param ticket_no the ticket_no to set
+     */
+    public void setTicket_no(String ticket_no) {
+        this.ticket_no = ticket_no;
+    }
+
+    /**
+     * @return the seat_no
+     */
+    public int getSeat_no() {
+        return seat_no;
+    }
+
+    /**
+     * @param seat_no the seat_no to set
+     */
+    public void setSeat_no(int seat_no) {
+        this.seat_no = seat_no;
+    }
+
+    /**
+     * @return the fair
+     */
+    public float getFair() {
+        return fair;
+    }
+
+    /**
+     * @param fair the fair to set
+     */
+    public void setFair(float fair) {
+        this.fair = fair;
+    }
+
+    /**
+     * @return the pass_no
+     */
+    public String getPass_no() {
+        return pass_no;
+    }
+
+    /**
+     * @param pass_no the pass_no to set
+     */
+    public void setPass_no(String pass_no) {
+        this.pass_no = pass_no;
+    }
+
+    /**
+     * @return the leg_no
+     */
+    public String getLeg_no() {
+        return leg_no;
+    }
+
+    /**
+     * @param leg_no the leg_no to set
+     */
+    public void setLeg_no(String leg_no) {
+        this.leg_no = leg_no;
+    }
+    
+    
+}
