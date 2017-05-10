b278ce4e7f7ae0307e4e5009e55cc4b57a88bf36
CSAbeywickrame <chaithikastephen1@gmail.com>
2017-05-10 20:49:59 +0530

    Getters and setters created for class AssignedTo

---
+++ air_ticket_reservation/src/Classes/AssignedTo.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+/**
+ *
+ * @author Chaithika
+ */
+public class AssignedTo {
+    private String leg_no;
+    private String flight_no;
+
+    /**
+     * @return the leg_no
+     */
+    public String getLeg_no() {
+        return leg_no;
+    }
+
+    /**
+     * @param leg_no the leg_no to set
+     */
+    public void setLeg_no(String leg_no) {
+        this.leg_no = leg_no;
+    }
+
+    /**
+     * @return the flight_no
+     */
+    public String getFlight_no() {
+        return flight_no;
+    }
+
+    /**
+     * @param flight_no the flight_no to set
+     */
+    public void setFlight_no(String flight_no) {
+        this.flight_no = flight_no;
+    }
+    
+    
+}
