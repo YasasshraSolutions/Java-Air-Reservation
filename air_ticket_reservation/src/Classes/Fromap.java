55604caf7f7493b187f047ff936260469e0312c5
CSAbeywickrame <chaithikastephen1@gmail.com>
2017-05-10 20:58:22 +0530

    Getters and setters created for class FromAp

---
+++ air_ticket_reservation/src/Classes/Fromap.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+/**
+ *
+ * @author Chaithika
+ */
+public class Fromap {
+    private String leg_no;
+    private String airportID;
+
+    /**
+     * @return the leg_no
+     */
+    public String getLeg_no() {
+        return leg_no;
+    }
+
+    /**
+     * @param leg_no the leg_no to set
+     */
+    public void setLeg_no(String leg_no) {
+        this.leg_no = leg_no;
+    }
+
+    /**
+     * @return the airportID
+     */
+    public String getAirportID() {
+        return airportID;
+    }
+
+    /**
+     * @param airportID the airportID to set
+     */
+    public void setAirportID(String airportID) {
+        this.airportID = airportID;
+    }
+    
+    
+}
