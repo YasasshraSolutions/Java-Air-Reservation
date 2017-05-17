df7f1d3efba4e477c44b7e0aeec9c16a49cf6410
manashg <manashgurudeniya@gmail.com>
2017-05-18 01:13:10 +0530

    no change just comitted

--- air_ticket_reservation/src/Interfaces/jTextField5.java
+++ air_ticket_reservation/src/Interfaces/jTextField5.java
@@ -10,5 +10,9 @@ package Interfaces;
  * @author Chaithika
  */
 class jTextField5 {
+
+    static void setText(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
